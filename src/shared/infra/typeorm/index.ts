8a0e23c93e75a462a09f4867aa738f5d8ad46b8c
Ronaldo Scheffer <r0n4ld0@gmail.com>
2021-02-26 15:16:25 -0300

    teste

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,5 +2,6 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (): Promise<Connection | undefined> => {
   const options = await getConnectionOptions();
+  console.log();
   return createConnection(options);
 };
