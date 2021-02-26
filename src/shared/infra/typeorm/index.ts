35c74ef895638f66ebbec625847e13d956850fb8
Ronaldo Scheffer <r0n4ld0@gmail.com>
2021-02-26 15:25:10 -0300

    Remove console.log

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,6 +2,5 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (): Promise<Connection | undefined> => {
   const options = await getConnectionOptions();
-  console.log();
   return createConnection(options);
 };
